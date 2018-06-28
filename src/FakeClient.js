ac7a660f89454af366a5e31ee296890d96cd3241
iosio <iosio.design@gmail.com>
2018-06-27 21:27:27 -0700

    quick fix

--- src/FakeClient.js
+++ src/FakeClient.js
@@ -57,33 +57,3 @@ export class FakeClient {
 
 }
 
-
-
-export class Client {
-    constructor(api_url) {
-        this.api_url = api_url;
-    }
-
-    get = (params) => fetch(`${this.api_url}${params ? params : ''}`)
-        .then((response) => this.handleError(response))
-        .then((response) => response.json());
-
-    handleError = (response) => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response;
-    };
-
-    post = (url, data) => fetch(`${this.api_url}${url ? url : ''}`,
-        {
-            mode: 'cors',
-            method: 'POST',
-            body: JSON.stringify(data)
-        })
-        .then((response) => {
-            console.log(response)
-            return this.handleError(response)
-        })
-        .then((response) => response.json());
-}
